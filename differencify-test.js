0e081af80664dc15fa9e94aa2a256fb5e670cce9
Vitalii Antoniuk <va@jelasric.com>
2019-01-18 12:58:59 +0200

    etalone test ready

--- differencify-test.js
+++ differencify-test.js
@@ -108,11 +108,11 @@ const sitemap = [
 ]
 
 const viewports = [
- /* { width: 1920, height: 1080 },
+  { width: 1920, height: 1080 },
   { width: 1400, height: 900 },
   { width: 1366, height: 768 },
   { width: 720, height: 1280 },
-  { width: 414, height: 736 },*/
+  { width: 414, height: 736 },
   { width: 360, height: 640 }
 ]
 
